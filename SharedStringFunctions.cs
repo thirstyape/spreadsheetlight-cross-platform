ba1fabb05da19b487d995fc7aed3e976c7f06417
Nathanael <nf@nathanaelfrey.ca>
2022-11-01 18:13:19 -0400

    Formatting updates

--- SharedStringFunctions.cs
+++ SharedStringFunctions.cs
@@ -1,129 +1,125 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using DocumentFormat.OpenXml;
+﻿using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 
-namespace SpreadsheetLight
+namespace SpreadsheetLight;
+
+public partial class SLDocument
 {
-    public partial class SLDocument
+    /// <summary>
+    /// Get existing shared strings. WARNING: This is only a snapshot. Any changes made to the returned result are not used.
+    /// </summary>
+    /// <returns>A list of existing shared strings.</returns>
+    public List<SLRstType> GetSharedStrings()
     {
-        /// <summary>
-        /// Get existing shared strings. WARNING: This is only a snapshot. Any changes made to the returned result are not used.
-        /// </summary>
-        /// <returns>A list of existing shared strings.</returns>
-        public List<SLRstType> GetSharedStrings()
+        List<SLRstType> result = new List<SLRstType>();
+        SLRstType rst = new SLRstType();
+        for (int i = 0; i < listSharedString.Count; ++i)
         {
-            List<SLRstType> result = new List<SLRstType>();
-            SLRstType rst = new SLRstType();
-            for (int i = 0; i < listSharedString.Count; ++i)
-            {
-                rst.FromHash(listSharedString[i]);
-                result.Add(rst.Clone());
-            }
-
-            return result;
+            rst.FromHash(listSharedString[i]);
+            result.Add(rst.Clone());
         }
 
-        /// <summary>
-        /// Get existing shared strings in a list of SharedStringItem objects. WARNING: This is only a snapshot. Any changes made to the returned result are not used.
-        /// </summary>
-        /// <returns>A list of existing SharedStringItem objects.</returns>
-        public List<SharedStringItem> GetSharedStringItems()
+        return result;
+    }
+
+    /// <summary>
+    /// Get existing shared strings in a list of SharedStringItem objects. WARNING: This is only a snapshot. Any changes made to the returned result are not used.
+    /// </summary>
+    /// <returns>A list of existing SharedStringItem objects.</returns>
+    public List<SharedStringItem> GetSharedStringItems()
+    {
+        List<SharedStringItem> result = new List<SharedStringItem>();
+        SharedStringItem ssi;
+
+        for (int i = 0; i < listSharedString.Count; ++i)
         {
-            List<SharedStringItem> result = new List<SharedStringItem>();
-            SharedStringItem ssi;
-
-            for (int i = 0; i < listSharedString.Count; ++i)
-            {
-                ssi = new SharedStringItem();
-                ssi.InnerXml = listSharedString[i];
-                result.Add(ssi);
-            }
-
-            return result;
+            ssi = new SharedStringItem();
+            ssi.InnerXml = listSharedString[i];
+            result.Add(ssi);
         }
 
-        internal void LoadSharedStringTable()
-        {
-            countSharedString = 0;
-            listSharedString = new List<string>();
-            dictSharedStringHash = new Dictionary<string, int>();
-            hsUniqueSharedString = new HashSet<string>();
+        return result;
+    }
 
-            if (wbp.SharedStringTablePart != null)
+    internal void LoadSharedStringTable()
+    {
+        countSharedString = 0;
+        listSharedString = new List<string>();
+        dictSharedStringHash = new Dictionary<string, int>();
+        hsUniqueSharedString = new HashSet<string>();
+
+        if (wbp.SharedStringTablePart != null)
+        {
+            OpenXmlReader oxr = OpenXmlReader.Create(wbp.SharedStringTablePart);
+            while (oxr.Read())
             {
-                OpenXmlReader oxr = OpenXmlReader.Create(wbp.SharedStringTablePart);
-                while (oxr.Read())
+                if (oxr.ElementType == typeof(SharedStringItem))
                 {
-                    if (oxr.ElementType == typeof(SharedStringItem))
-                    {
-                        this.ForceSaveToSharedStringTable((SharedStringItem)oxr.LoadCurrentElement());
-                    }
-                }
-                oxr.Dispose();
-
-                countSharedString = listSharedString.Count;
-            }
-        }
-
-        internal void WriteSharedStringTable()
-        {
-            if (wbp.SharedStringTablePart != null)
-            {
-                if (listSharedString.Count > countSharedString)
-                {
-                    if (gbWriteUniqueSharedStringCount)
-                    {
-                        wbp.SharedStringTablePart.SharedStringTable.Count = (uint)listSharedString.Count;
-                        wbp.SharedStringTablePart.SharedStringTable.UniqueCount = (uint)hsUniqueSharedString.Count;
-                    }
-                    else
-                    {
-                        wbp.SharedStringTablePart.SharedStringTable.Count = null;
-                        wbp.SharedStringTablePart.SharedStringTable.UniqueCount = null;
-                    }
-
-                    int diff = listSharedString.Count - countSharedString;
-                    for (int i = 0; i < diff; ++i)
-                    {
-                        wbp.SharedStringTablePart.SharedStringTable.Append(new SharedStringItem()
-                        {
-                            InnerXml = listSharedString[i + countSharedString]
-                        });
-                    }
-
-                    wbp.SharedStringTablePart.SharedStringTable.Save();
+                    this.ForceSaveToSharedStringTable((SharedStringItem)oxr.LoadCurrentElement());
                 }
             }
-            else
+            oxr.Dispose();
+
+            countSharedString = listSharedString.Count;
+        }
+    }
+
+    internal void WriteSharedStringTable()
+    {
+        if (wbp.SharedStringTablePart != null)
+        {
+            if (listSharedString.Count > countSharedString)
             {
-                if (listSharedString.Count > 0)
+                if (gbWriteUniqueSharedStringCount)
                 {
-                    SharedStringTablePart sstp = wbp.AddNewPart<SharedStringTablePart>();
-                    using (MemoryStream ms = new MemoryStream())
+                    wbp.SharedStringTablePart.SharedStringTable.Count = (uint)listSharedString.Count;
+                    wbp.SharedStringTablePart.SharedStringTable.UniqueCount = (uint)hsUniqueSharedString.Count;
+                }
+                else
+                {
+                    wbp.SharedStringTablePart.SharedStringTable.Count = null;
+                    wbp.SharedStringTablePart.SharedStringTable.UniqueCount = null;
+                }
+
+                int diff = listSharedString.Count - countSharedString;
+                for (int i = 0; i < diff; ++i)
+                {
+                    wbp.SharedStringTablePart.SharedStringTable.Append(new SharedStringItem()
                     {
-                        using (StreamWriter sw = new StreamWriter(ms))
+                        InnerXml = listSharedString[i + countSharedString]
+                    });
+                }
+
+                wbp.SharedStringTablePart.SharedStringTable.Save();
+            }
+        }
+        else
+        {
+            if (listSharedString.Count > 0)
+            {
+                SharedStringTablePart sstp = wbp.AddNewPart<SharedStringTablePart>();
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (StreamWriter sw = new StreamWriter(ms))
+                    {
+                        if (gbWriteUniqueSharedStringCount)
                         {
-                            if (gbWriteUniqueSharedStringCount)
-                            {
-                                sw.Write("<x:sst count=\"{0}\" uniqueCount=\"{1}\" xmlns:x=\"{2}\">", listSharedString.Count, hsUniqueSharedString.Count, SLConstants.NamespaceX);
-                            }
-                            else
-                            {
-                                sw.Write("<x:sst xmlns:x=\"{0}\">", SLConstants.NamespaceX);
-                            }
-                            
-                            for (int i = 0; i < listSharedString.Count; ++i)
-                            {
-                                sw.Write("<x:si>{0}</x:si>", listSharedString[i]);
-                            }
-                            sw.Write("</x:sst>");
-                            sw.Flush();
-                            ms.Position = 0;
-                            sstp.FeedData(ms);
+                            sw.Write("<x:sst count=\"{0}\" uniqueCount=\"{1}\" xmlns:x=\"{2}\">", listSharedString.Count, hsUniqueSharedString.Count, SLConstants.NamespaceX);
                         }
+                        else
+                        {
+                            sw.Write("<x:sst xmlns:x=\"{0}\">", SLConstants.NamespaceX);
+                        }
+                        
+                        for (int i = 0; i < listSharedString.Count; ++i)
+                        {
+                            sw.Write("<x:si>{0}</x:si>", listSharedString[i]);
+                        }
+                        sw.Write("</x:sst>");
+                        sw.Flush();
+                        ms.Position = 0;
+                        sstp.FeedData(ms);
                     }
                 }
             }
--- office2010/SLColorScale2010.cs
+++ office2010/SLColorScale2010.cs
@@ -1,87 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using DocumentFormat.OpenXml;
+﻿using DocumentFormat.OpenXml;
 using X14 = DocumentFormat.OpenXml.Office2010.Excel;
 
-namespace SpreadsheetLight
+namespace SpreadsheetLight;
+
+internal class SLColorScale2010
 {
-    internal class SLColorScale2010
+    //http://msdn.microsoft.com/en-us/library/documentformat.openxml.office2010.excel.colorscale.aspx
+
+    internal List<SLConditionalFormattingValueObject2010> Cfvos { get; set; }
+    internal List<SLColor> Colors { get; set; }
+
+    internal SLColorScale2010()
     {
-        //http://msdn.microsoft.com/en-us/library/documentformat.openxml.office2010.excel.colorscale.aspx
+        this.SetAllNull();
+    }
 
-        internal List<SLConditionalFormattingValueObject2010> Cfvos { get; set; }
-        internal List<SLColor> Colors { get; set; }
+    private void SetAllNull()
+    {
+        this.Cfvos = new List<SLConditionalFormattingValueObject2010>();
+        this.Colors = new List<SLColor>();
+    }
 
-        internal SLColorScale2010()
+    internal void FromColorScale(X14.ColorScale cs)
+    {
+        this.SetAllNull();
+
+        SLConditionalFormattingValueObject2010 cfvo;
+        SLColor clr;
+        using (OpenXmlReader oxr = OpenXmlReader.Create(cs))
         {
-            this.SetAllNull();
-        }
-
-        private void SetAllNull()
-        {
-            this.Cfvos = new List<SLConditionalFormattingValueObject2010>();
-            this.Colors = new List<SLColor>();
-        }
-
-        internal void FromColorScale(X14.ColorScale cs)
-        {
-            this.SetAllNull();
-
-            SLConditionalFormattingValueObject2010 cfvo;
-            SLColor clr;
-            using (OpenXmlReader oxr = OpenXmlReader.Create(cs))
+            while (oxr.Read())
             {
-                while (oxr.Read())
+                if (oxr.ElementType == typeof(X14.ConditionalFormattingValueObject))
                 {
-                    if (oxr.ElementType == typeof(X14.ConditionalFormattingValueObject))
-                    {
-                        cfvo = new SLConditionalFormattingValueObject2010();
-                        cfvo.FromConditionalFormattingValueObject((X14.ConditionalFormattingValueObject)oxr.LoadCurrentElement());
-                        this.Cfvos.Add(cfvo);
-                    }
-                    else if (oxr.ElementType == typeof(X14.Color))
-                    {
-                        clr = new SLColor(new List<System.Drawing.Color>(), new List<System.Drawing.Color>());
-                        clr.FromExcel2010Color((X14.Color)oxr.LoadCurrentElement());
-                        this.Colors.Add(clr);
-                    }
+                    cfvo = new SLConditionalFormattingValueObject2010();
+                    cfvo.FromConditionalFormattingValueObject((X14.ConditionalFormattingValueObject)oxr.LoadCurrentElement());
+                    this.Cfvos.Add(cfvo);
+                }
+                else if (oxr.ElementType == typeof(X14.Color))
+                {
+                    clr = new SLColor(new List<System.Drawing.Color>(), new List<System.Drawing.Color>());
+                    clr.FromExcel2010Color((X14.Color)oxr.LoadCurrentElement());
+                    this.Colors.Add(clr);
                 }
             }
         }
+    }
 
-        internal X14.ColorScale ToColorScale()
+    internal X14.ColorScale ToColorScale()
+    {
+        X14.ColorScale cs = new X14.ColorScale();
+        foreach (SLConditionalFormattingValueObject2010 cfvo in this.Cfvos)
         {
-            X14.ColorScale cs = new X14.ColorScale();
-            foreach (SLConditionalFormattingValueObject2010 cfvo in this.Cfvos)
-            {
-                cs.Append(cfvo.ToConditionalFormattingValueObject());
-            }
-            foreach (SLColor clr in this.Colors)
-            {
-                cs.Append(clr.ToExcel2010Color());
-            }
-
-            return cs;
+            cs.Append(cfvo.ToConditionalFormattingValueObject());
+        }
+        foreach (SLColor clr in this.Colors)
+        {
+            cs.Append(clr.ToExcel2010Color());
         }
 
-        internal SLColorScale2010 Clone()
+        return cs;
+    }
+
+    internal SLColorScale2010 Clone()
+    {
+        SLColorScale2010 cs = new SLColorScale2010();
+
+        int i;
+        cs.Cfvos = new List<SLConditionalFormattingValueObject2010>();
+        for (i = 0; i < this.Cfvos.Count; ++i)
         {
-            SLColorScale2010 cs = new SLColorScale2010();
-
-            int i;
-            cs.Cfvos = new List<SLConditionalFormattingValueObject2010>();
-            for (i = 0; i < this.Cfvos.Count; ++i)
-            {
-                cs.Cfvos.Add(this.Cfvos[i].Clone());
-            }
-
-            cs.Colors = new List<SLColor>();
-            for (i = 0; i < this.Colors.Count; ++i)
-            {
-                cs.Colors.Add(this.Colors[i].Clone());
-            }
-
-            return cs;
+            cs.Cfvos.Add(this.Cfvos[i].Clone());
         }
+
+        cs.Colors = new List<SLColor>();
+        for (i = 0; i < this.Colors.Count; ++i)
+        {
+            cs.Colors.Add(this.Colors[i].Clone());
+        }
+
+        return cs;
     }
 }
